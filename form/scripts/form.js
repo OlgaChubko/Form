c85b898ace16cd38821a8aded5edd42e0cb5864d
olga <olga.chubko.92@mail.ru>
2017-05-08 11:28:48 +0300

    first commit( without button)

---
+++ form/scripts/form.js
@@ -0,0 +1,34 @@
+const LOGIN_CHECK_RULE = /^[-._a-z0-9]+@(?:[a-z0-9][-a-z0-9]+\.)+[a-z]{2,6}$/;
+const PASSWORD_CHECK_RULE = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/;
+var LOGIN_CORRECT;
+var PASSWORD_CORRECT;
+
+function validationInputData(inputType){
+    if(inputType == 'login'){
+        LOGIN_CORRECT = LOGIN_CHECK_RULE.test(document.getElementById('login').value);
+        highlightingInput(LOGIN_CORRECT);
+    }else if(inputType == 'password'){
+        PASSWORD_CORRECT = PASSWORD_CHECK_RULE.test(document.getElementById('password').value);
+        highlightingInput(PASSWORD_CORRECT);
+    }
+
+    function highlightingInput(choice){
+        if (choice) {
+            document.getElementById(inputType).style.borderColor = '#9EFFB6';
+            document.getElementById(inputType + "Error").style.display = 'none';
+        } else {
+            event.preventDefault();
+            document.getElementById(inputType).style.borderColor = 'red';
+            document.getElementById(inputType + "Error").style.display = 'block';
+        }
+    }
+}
+
+
+function buttonLock(){
+    if(LOGIN_CORRECT == false && PASSWORD_CORRECT == false){
+        alert('no');
+        document.getElementById('button').setAttribute("disabled", "disabled");
+    }}
+
+
